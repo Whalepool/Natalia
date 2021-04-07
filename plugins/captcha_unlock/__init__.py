c2b737baa87f1bbae3c1e40e42ebf59a060165da
ntom <flibbr@gmail.com>
2021-04-07 15:16:10 +0100

    bugfix

--- plugins/captcha_unlock/__init__.py
+++ plugins/captcha_unlock/__init__.py
@@ -106,7 +106,7 @@ class Captcha_Unlock:
                                     can_send_polls=True, 
                                     can_send_other_messages=True,
                                     can_add_web_page_previews=True, 
-                                    can_change_info=True,
+                                    can_change_info=False,
                                     can_invite_users=True, 
                                     can_pin_messages=False
                                 )
